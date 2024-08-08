8f0d94db86e03a407d706f75541cb83a399d8e36
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-08-09 00:47:54 +0900

    added base code

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from src.functions import compare_resume
+
+
+
+def main():
+    # Title
+    st.sidebar.title("ResumeFit")
+    st.sidebar.write("""Fill in the fields below to compare a resume to a job description.""")
+
+    # Input fields
+    resume_text = st.sidebar.text_area("Resume Information", height=200)
+    jd_text = st.sidebar.text_area("Job Description", height=200)
+
+    # Submit button
+    if st.sidebar.button("Submit"):
+        # Process the inputs
+        st.session_state.resume_text = resume_text
+        st.session_state.jd_text = jd_text
+        st.header("Fit Score")
+        output = compare_resume(resume_text, jd_text)
+        process_inputs(output)
+
+
+def process_inputs(input1):
+    # Function to display the final output
+    # Process the inputs here
+    st.write(" ", input1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/functions.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+# # for testing locally --------------------------------------
+# load_dotenv()
+# goog_api_key = os.getenv('GOOGLE_API_KEY')
+
+# for testing on streamlit share -----------------------------
+goog_api_key = st.secrets['GOOGLE_API_KEY']
+
+def compare_resume(resume_text, jd_text):
+    model = genai.GenerativeModel('gemini-1.5-flash')
+
+    response = model.generate_content(f"""
+    Resume: ```{resume_text}```
+    Job Description: ```{jd_text}```
+    Given the above resume and job description delineated by ```, identify the skills and qualifications from both.
+    Compare them to determine any skill gaps and estimate how qualified the individual is for the job.
+    Give a percentage estimating how qualified the individual is for the job.
+    For candidate information section, please cite any applicable certifications they have.
+    Please penalize heavily for any missing mandatory qualifications and reflect it in the percentage.
+
+    Output format should be as below, with each section title in large font:
+
+    Estimated qualification percentage:
+
+    Candidate information section
+    Current location:
+    College degree (Bachelor's or above):
+    Japanese language ability:
+    English language ability:
+
+    Summary:
+    (please give a short summary of candidate persona.
+    example: junior level candidate with 2 years of experience in software engineering, proficient in Python, Java, and C++)
+
+    Top 5 Skills and qualifications from resume:
+    - skill1
+    - skill2
+    - skill3
+    ...
+    5 main skills and qualifications from job description:
+    - skill1
+    - skill2
+    - skill3
+    ...
+    Skill gaps:
+    - skill1
+    - skill2
+    - skill3
+    ...
+
+    If the candidate fits any of the conditions, please give a warning as being ineligible:
+    - if the candidate is overseas AND has no university degree
+
+    If the candidate fits any of the conditions, please give a warning as being unqualified
+    - if the position requires Japanese language ability AND the candidate has no Japanese language ability
+    - if the position requires English language ability AND the candidate has no English language ability
+    - if
+    """)
+
+
+    answer = response.text
+
+
+    return answer
