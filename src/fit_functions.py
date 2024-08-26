9f085cbf1dc6279994605ffbb77ac0a6134b8f0e
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-08-27 02:15:12 +0900

    made headers smaller, re-ordered and renamed match %

--- src/fit_functions.py
+++ src/fit_functions.py
@@ -36,91 +36,6 @@ Functions requiring OpenAI API
 # for testing on streamlit share -----------------------------
 api_key = st.secrets['OPENAI_API_KEY']
 
-# def compare_resume(resume_text, jd_text):
-
-#     # streamlit
-#     client = OpenAI()
-
-#     completion = client.chat.completions.create(
-#         model="gpt-4o-mini",
-#         messages=[
-#             {"role": "system",
-#              "content": "You are a tech recruiter screening resumes."
-#             },
-#             {
-#                 "role": "user",
-#                 "content": f"""
-#                 Resume: ```{resume_text}```
-#                 Job Description: ```{jd_text}```
-#                 Given the above resume and job description delineated by ```, identify the skills and qualifications from both.
-#                 Compare them to determine any skill gaps and estimate how qualified the individual is for the job.
-#                 Give a percentage estimating how qualified the individual is for the job.
-
-#                 Please penalize heavily for any missing mandatory qualifications.
-#                 If the candidate is missing any mandatory qualifications, please also give a warning.
-
-#                 If any of the conditionsa are true, please give warnings for the ones that are true:
-#                 - if candidate is missing mandatory qualifications: "Candidate may be missing mandatory qualifications. Please review carefully."
-#                 - if the candidate has no university degree: "Candidate may not have university degree. Highest education listed is [highest education listed]." (example: if high school diploma is highest mentioned, assume they do not have college degree) (if resume does not mention a university degree, assume they don't have one)
-#                 - if candidate is outside of Japan, please give a warning: "Candidate may be overseas. Beware of hiring timelines and visa eligibility." (assume that their last place of work is their current location)
-
-
-#                 Output format should be as below, with each section title in large font.
-#                 Please fill in the blanks with the appropriate information.
-#                 Please use new lines appropriately like in the format below.
-
-#                 OUTPUT FORMAT:
-#                 ```
-
-#                 ### Candidate Information
-#                 - Current location:
-#                 - College degree (Bachelor's or above):
-#                 - Japanese language ability:
-#                 - English language ability:
-
-#                 ### Warnings:
-#                 ⚠️(if mandatory qualifications are missing, give a warning here)
-#                 ⚠️(if no university degree is not listed, give a warning here)
-#                 ⚠️(if candidate is not in Japan, give a warning here)
-#                 (if none of the above apply, write "None.")
-
-#                 ## Estimated qualification percentage: [percentage]
-#                 ### Analysis:
-#                 [reason why you gave the percentage]
-
-#                 ### Candidate Summary:
-#                 (please give a short summary of candidate's experiences and skills.
-#                 example: junior level candidate with 2 years of experience in software engineering, proficient in Python, Java, and C++)
-
-#                 ### Qualifications:
-#                 [✅/❌] [Qualification 1]: [What you can tell from the resume]  \n
-#                 [✅/❌] [Qualification 2]: [What you can tell from the resume]  \n
-#                 etc.  \n
-
-#                 ### Nice-to-have:
-#                 [✅/❌] [Nice-to-have 1]: [What you can tell from the resume]  \n
-#                 [✅/❌] [Nice-to-have 2]: [What you can tell from the resume]  \n
-#                 etc.  \n
-
-#                 ### Skill gaps:
-#                 [write any notable skill gaps here]
-
-#                 ...
-#                 ```
-
-
-#                 """
-#             }
-#         ]
-#     )
-
-#     print(completion)
-#     print(completion.choices[0].message)
-
-#     content = completion.choices[0].message.content
-
-#     return content
-
 
 
 
@@ -162,7 +77,11 @@ def compare_resume(resume_text, jd_text):
 
                 OUTPUT FORMAT:
 
-                <h2>Candidate Information</h2>
+                <h2>Estimated Match: [percentage]</h2>
+                <h4>Analysis:</h4>
+                <p>[reason why you gave the percentage]</p>
+
+                <h4>Candidate Information</h4>
                 <ul>
                     <li>Current location:</li>
                     <li>College degree (Bachelor's or above):</li>
@@ -170,7 +89,7 @@ def compare_resume(resume_text, jd_text):
                     <li>English language ability:</li>
                 </ul>
 
-                <h2>Warnings:</h2>
+                <h4>Warnings:</h4>
                 <ul>
                     <li>⚠️ (if mandatory qualifications are missing, give a warning here)</li>
                     <li>⚠️ (if no university degree is not listed, give a warning here)</li>
@@ -178,28 +97,24 @@ def compare_resume(resume_text, jd_text):
                     <li>(if none of the above apply, write "None.")</li>
                 </ul>
 
-                <h2>Estimated qualification percentage: [percentage]</h2>
-                <h3>Analysis:</h3>
-                <p>[reason why you gave the percentage]</p>
-
-                <h3>Candidate Summary:</h3>
+                <h4>Candidate Summary:</h4>
                 <p>(please give a short summary of candidate's experiences and skills. example: junior level candidate with 2 years of experience in software engineering, proficient in Python, Java, and C++)</p>
 
-                <h3>Qualifications:</h3>
+                <h4>Qualifications:</h4>
                 <ul>
                     <li>[✅/❌] [Qualification 1]: [What you can tell from the resume]</li>
                     <li>[✅/❌] [Qualification 2]: [What you can tell from the resume]</li>
                     <li>etc.</li>
                 </ul>
 
-                <h3>Nice-to-have:</h3>
+                <h4>Nice-to-have:</h4>
                 <ul>
                     <li>[✅/❌] [Nice-to-have 1]: [What you can tell from the resume]</li>
                     <li>[✅/❌] [Nice-to-have 2]: [What you can tell from the resume]</li>
                     <li>etc.</li>
                 </ul>
 
-                <h3>Skill gaps:</h3>
+                <h4>Skill gaps:</h4>
                 <p>[write any notable skill gaps here]</p>
 
 
