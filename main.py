18fba7c85ad3892aa1686a8b6f0a495b6b48521e
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-08-27 01:15:18 +0900

    fixed params in st.markdown

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def main():
 def process_inputs(input1):
     # Function to display the final output
     # Process the inputs here
-    st.markdown(" ", input1)
+    st.markdown(input1)
 
 
 if __name__ == "__main__":
