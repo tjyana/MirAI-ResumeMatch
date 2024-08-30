4fded9bf24b9b815e9c04f2909a2d86486b70425
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-08-31 03:03:32 +0900

    conditional rendering to get rid of the None on main screen

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ def main():
         resume_text = resume_input()
         jd_text = jd_input()
         output = submit_button(resume_text, jd_text, language)
-        process_inputs(output)
+        if output:
+            process_inputs(output)
 
     # Japanese version
     elif language == '日本語':
@@ -23,42 +24,8 @@ def main():
         resume_text = JP_resume_input()
         jd_text = JP_jd_input()
         output = JP_submit_button(resume_text, jd_text, language)
-        process_inputs(output)
-
-
-    # # Title
-    # title = '''
-    # [MirAI Fest Entry]
-    # # :orange[ResumeMatch]✅
-    # ### Compare a resume to a job description!
-    # '''
-    # st.sidebar.markdown(title, unsafe_allow_html=True)
-
-    # # Resume
-    # st.sidebar.header("Resume")
-
-    # # Select input method: Copy and paste text or upload a file
-    # resume_method = st.sidebar.radio("""Choose Resume input method:""", ("File", "Text"), horizontal = True)
-
-    # # Input: Text
-    # if resume_method == "Text":
-    #     resume_text = st.sidebar.text_area("Paste Resume text", height=200)
-    # # Input: File Upload
-    # elif resume_method == "File":
-    #     resume_file = st.sidebar.file_uploader("Upload Resume file", type=["pdf", "docx", "txt"])
-    #     if resume_file:
-    #         resume_text = read_resume(resume_file)
-
-    # # Input: Job Description
-    # st.sidebar.header("Job Description")
-    # jd_text = st.sidebar.text_area("Paste JD text", height=200)
-
-    # # Submit button
-    # if st.sidebar.button("Submit"):
-    #     st.header("Match Results")
-    #     output = compare_resume(resume_text, jd_text)
-    #     process_inputs(output)
-
+        if output:
+            process_inputs(output)
 
 
 def process_inputs(input1):
