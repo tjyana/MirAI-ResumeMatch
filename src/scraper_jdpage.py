574c9887074458a709846dadae8e0f516af4ca80
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-09-01 00:11:33 +0900

    separated out get_url an added to scrape_jd

--- src/scraper_jdpage.py
+++ src/scraper_jdpage.py
@@ -7,50 +7,40 @@ import streamlit as st
 url = 'https://hrmos.co/pages/moneyforward/jobs/1877612029521268793'
 
 
-def scrape_description(url):
 
-    if 'hrmos.co/pages/moneyforward/jobs/' not in url:
-        st.error('Invalid URL. Please enter a valid MoneyForward job description.')
-        return None
+def get_url(url):
+    # Fetch the main page
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    return soup
 
+
+def scrape_description(soup):
+
+    # Find all job entries
+    job_description_html = str(soup.find_all('div', class_='pg-main-column cf'))  # Adjust class name as needed
+    # Find the position of the phrase "職種 / 募集ポジション"
+    cutoff_position = job_description_html.find('職種 / 募集ポジション')
+
+    # Truncate the content after the phrase
+    if cutoff_position != -1:
+        truncated_description = job_description_html[:cutoff_position]
     else:
-        # Fetch the main page
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
+        truncated_description = job_description_html
 
-        # Find all job entries
-        job_description_html = str(soup.find_all('div', class_='pg-main-column cf'))  # Adjust class name as needed
-        # Find the position of the phrase "職種 / 募集ポジション"
-        cutoff_position = job_description_html.find('職種 / 募集ポジション')
+    # Convert the truncated HTML back to text
+    soup_truncated = BeautifulSoup(truncated_description, 'html.parser')
+    jd_text = soup_truncated.get_text(separator='\n').strip()
 
-        # Truncate the content after the phrase
-        if cutoff_position != -1:
-            truncated_description = job_description_html[:cutoff_position]
-        else:
-            truncated_description = job_description_html
-
-        # Convert the truncated HTML back to text
-        soup_truncated = BeautifulSoup(truncated_description, 'html.parser')
-        jd_text = soup_truncated.get_text(separator='\n').strip()
-
-        return jd_text
+    return jd_text
 
 
-def scrape_title(url):
+def scrape_title(soup):
 
+    # Find the h1 element with the class sg-corporate-name
+    job_title = soup.find('h1', class_='sg-corporate-name').text
 
-    if 'hrmos.co/pages/moneyforward/jobs/' not in url:
-        st.error('Invalid URL. Please enter a valid MoneyForward job description.')
-        return None
-
-    else:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-
-        # Find the h1 element with the class sg-corporate-name
-        job_title = soup.find('h1', class_='sg-corporate-name').text
-
-        return job_title
+    return job_title
 
 
 # Orchestrating the scraping functions
@@ -63,6 +53,7 @@ def scrape_jd(url):
 
     # Else, scrape the job description
     else:
-        jd_text = scrape_description(url)
-        job_title = scrape_title(url)
+        soup = get_url(url)
+        jd_text = scrape_description(soup)
+        job_title = scrape_title(soup)
         return jd_text, job_title
