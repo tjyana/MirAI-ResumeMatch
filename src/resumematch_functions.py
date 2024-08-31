f13cea64ed3aaf09671b955cd4bf48a292ec0ab7
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-08-31 16:15:45 +0900

    move spinner up

--- src/resumematch_functions.py
+++ src/resumematch_functions.py
@@ -126,88 +126,86 @@ def compare_resume(resume_text, jd_text, language):
     Final HTML output version of the function
     '''
 
-    # streamlit
-    client = OpenAI()
+    with st.spinner('ResumeMatching...'):
 
-    completion = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "system",
-             "content": "You are a tech recruiter screening resumes."
-            },
-            {
-                "role": "user",
-                "content": f"""
-                Resume: ```{resume_text}```
-                Job Description: ```{jd_text}```
-                Given the above resume and job description delineated by ```, identify the skills and qualifications from both.
-                Compare them to determine any skill gaps and estimate how qualified the individual is for the job.
-                Give a percentage estimating how qualified the individual is for the job.
+        client = OpenAI()
 
-                Please penalize heavily for any missing mandatory qualifications.
-                If the candidate is missing any mandatory qualifications, please also give a warning.
+        completion = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "system",
+                "content": "You are a tech recruiter screening resumes."
+                },
+                {
+                    "role": "user",
+                    "content": f"""
+                    Resume: ```{resume_text}```
+                    Job Description: ```{jd_text}```
+                    Given the above resume and job description delineated by ```, identify the skills and qualifications from both.
+                    Compare them to determine any skill gaps and estimate how qualified the individual is for the job.
+                    Give a percentage estimating how qualified the individual is for the job.
 
-                If any of the conditionsa are true, please give warnings for the ones that are true:
-                - if candidate is missing mandatory qualifications: "Candidate may be missing mandatory qualifications. Please review carefully."
-                - if the candidate has no university degree: "Candidate may not have university degree. Highest education listed is [highest education listed]." (example: if high school diploma is highest mentioned, assume they do not have college degree) (if resume does not mention a university degree, assume they don't have one)
-                - if candidate is outside of Japan, please give a warning: "Candidate may be overseas. Beware of hiring timelines and visa eligibility." (assume that their last place of work is their current location)
-                - if you cannot confirm candidate fulfills jd language requirements or if resume and job description are in different languages, please give a warning: "Language requirements are not confirmed. Please verify before proceeding."
+                    Please penalize heavily for any missing mandatory qualifications.
+                    If the candidate is missing any mandatory qualifications, please also give a warning.
 
-                Output format should be as below, with each section title in large font.
-                Please fill in the blanks with the appropriate information.
-                Please make sure output language is in {language}.
+                    If any of the conditionsa are true, please give warnings for the ones that are true:
+                    - if candidate is missing mandatory qualifications: "Candidate may be missing mandatory qualifications. Please review carefully."
+                    - if the candidate has no university degree: "Candidate may not have university degree. Highest education listed is [highest education listed]." (example: if high school diploma is highest mentioned, assume they do not have college degree) (if resume does not mention a university degree, assume they don't have one)
+                    - if candidate is outside of Japan, please give a warning: "Candidate may be overseas. Beware of hiring timelines and visa eligibility." (assume that their last place of work is their current location)
+                    - if you cannot confirm candidate fulfills jd language requirements or if resume and job description are in different languages, please give a warning: "Language requirements are not confirmed. Please verify before proceeding."
 
-                OUTPUT FORMAT:
+                    Output format should be as below, with each section title in large font.
+                    Please fill in the blanks with the appropriate information.
+                    Please make sure output language is in {language}.
 
-                <h2>Estimated Match: [percentage]</h2>
+                    OUTPUT FORMAT:
 
-                <ul>
-                    <li>⚠️ (if mandatory qualifications are missing, give a warning here)</li>
-                    <li>⚠️ (if no university degree is not listed, give a warning here)</li>
-                    <li>⚠️ (if candidate is not in Japan, give a warning here)</li>
-                    <li>⚠️ (if language requirements are not confirmed, give a warning here)</li>
-                </ul>
+                    <h2>Estimated Match: [percentage]</h2>
 
-                <h4>Job Description Summary:</h5>
-                <p>[Title of the job]: [One sentence summary of the job description.]</p>
+                    <ul>
+                        <li>⚠️ (if mandatory qualifications are missing, give a warning here)</li>
+                        <li>⚠️ (if no university degree is not listed, give a warning here)</li>
+                        <li>⚠️ (if candidate is not in Japan, give a warning here)</li>
+                        <li>⚠️ (if language requirements are not confirmed, give a warning here)</li>
+                    </ul>
 
-                <h4>Resume Summary:</h5>
-                <ul>
-                    <li>[Summary of the resume in one sentence. Please include years of experience if discernible.]</li>
-                    <li><b>Current location:</b> [current location] </li>
-                    <li><b>Highest eduation:</b> [highest education listed] [</li>
-                    <li><b>Japanese language ability:</b> [Japanese language ability discernible from resume] </li>
-                    <li><b>English language ability:</b> [English language ability discernible from resume</li>
-                </ul>
+                    <h4>Job Description Summary:</h5>
+                    <p>[Title of the job]: [One sentence summary of the job description.]</p>
 
-                <h3>Comparison Analysis:</h3>
-                [reason why you gave the percentage]
+                    <h4>Resume Summary:</h5>
+                    <ul>
+                        <li>[Summary of the resume in one sentence. Please include years of experience if discernible.]</li>
+                        <li><b>Current location:</b> [current location] </li>
+                        <li><b>Highest eduation:</b> [highest education listed] [</li>
+                        <li><b>Japanese language ability:</b> [Japanese language ability discernible from resume] </li>
+                        <li><b>English language ability:</b> [English language ability discernible from resume</li>
+                    </ul>
 
-                <h4>Qualifications:</h4>
-                <ul>
-                    <li>[✅/❌] [Qualification 1]: [What you can tell from the resume]</li>
-                    <li>[✅/❌] [Qualification 2]: [What you can tell from the resume]</li>
-                    <li>etc.</li>
-                </ul>
+                    <h3>Comparison Analysis:</h3>
+                    [reason why you gave the percentage]
 
-                <h4>Nice-to-have:</h4>
-                <ul>
-                    <li>[✅/❌] [Nice-to-have 1]: [What you can tell from the resume]</li>
-                    <li>[✅/❌] [Nice-to-have 2]: [What you can tell from the resume]</li>
-                    <li>etc.</li>
-                </ul>
+                    <h4>Qualifications:</h4>
+                    <ul>
+                        <li>[✅/❌] [Qualification 1]: [What you can tell from the resume]</li>
+                        <li>[✅/❌] [Qualification 2]: [What you can tell from the resume]</li>
+                        <li>etc.</li>
+                    </ul>
+
+                    <h4>Nice-to-have:</h4>
+                    <ul>
+                        <li>[✅/❌] [Nice-to-have 1]: [What you can tell from the resume]</li>
+                        <li>[✅/❌] [Nice-to-have 2]: [What you can tell from the resume]</li>
+                        <li>etc.</li>
+                    </ul>
 
 
 
 
-                """
-            }
-        ]
-    )
+                    """
+                }
+            ]
+        )
 
-    print(completion)
-    print(completion.choices[0].message)
-
-    content = completion.choices[0].message.content
+        content = completion.choices[0].message.content
 
     return content
