6aac2463d25386d8d5e80b42f893f83615999c34
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-08-31 19:36:27 +0900

    updated JP to match EN

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pdfplumber
-from src.resumematch_functions import compare_resume, JP_compare_resume
+from src.resumematch_functions import compare_resume
 from src.st_functions import read_resume, language_options, UI, resume_input, jd_input, submit_button
 from src.st_functions_JP import read_resume, JP_UI, JP_resume_input, JP_jd_input, JP_submit_button
 
@@ -20,17 +20,24 @@ def main():
 
     # Japanese version
     elif language == '日本語':
-        JP_UI()
-        resume_text = JP_resume_input()
-        jd_text = JP_jd_input()
-        output = JP_submit_button(resume_text, jd_text, language)
-        if output:
-            process_inputs(output)
+        output = JP_UI(language)
+
+    # # Japanese version
+    # elif language == '日本語':
+    #     JP_UI()
+    #     resume_text = JP_resume_input()
+    #     jd_text = JP_jd_input()
+    #     output = JP_submit_button(resume_text, jd_text, language)
+    #     if output:
+    #         process_inputs(output)
+
+    if output:
+        st.markdown(output, unsafe_allow_html=True)
 
 
-def process_inputs(input1):
-    # Function to display the final output
-    st.markdown(input1, unsafe_allow_html=True)
+# def process_inputs(input1):
+#     # Function to display the final output
+#     st.markdown(input1, unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
