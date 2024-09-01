5ea7ad63b244f254ae787a99c01e57e8e78a9480
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-09-01 14:44:34 +0900

    implemented try/except block

--- src/st_functions.py
+++ src/st_functions.py
@@ -27,13 +27,16 @@ def resume_input():
 
     # Input: Text
     if resume_method == "Text":
-        resume_text = st.sidebar.text_area("Paste Resume text")
+        resume_text = st.sidebar.text_area("Enter Resume text")
+        if resume_text:
+            st.write(f'Resume: Text input')
         return resume_text
     # Input: File Upload
     elif resume_method == "File":
         resume_file = st.sidebar.file_uploader("Upload Resume file", type=["pdf", "docx", "txt"])
         if resume_file:
             resume_text = read_resume(resume_file)
+            st.write(f'Resume: File upload')
             return resume_text
 
 
@@ -42,23 +45,35 @@ def jd_input():
     st.sidebar.header("Job Description")
 
     # Select input method:
-    jd_method = st.sidebar.radio("""Choose JD input method:""", ("Link", "Text"), horizontal = True)
-
-    # Input: Text
-    if jd_method == "Text":
-        jd_text = st.sidebar.text_area("Paste JD text")
-        jd_title = "n/a"
-        return jd_title, jd_text
+    jd_method = st.sidebar.radio("""Choose JD input method:""", ("JD Link", "Text"), horizontal = True)
 
     # Input: Link
-    elif jd_method == "Link":
-        jd_link = st.sidebar.text_input("Paste JD link")
-        if jd_link != "":
-            jd_title, jd_text = scrape_jd(jd_link)
-            return jd_title, jd_text
+    if jd_method == "JD Link":
+        jd_link = st.sidebar.text_input("Enter JD link")
+        if jd_link:
+            try:
+                jd_title, jd_text = scrape_jd(jd_link)
+                st.write(f'''Job Description: Link input \n
+                         Job Title: {jd_title}''')
+                return jd_title, jd_text
+            except Exception as e:
+                st.error("Could not retrieve job information from the URL. Please check the URL.")
+                return None, None
+
+    # Input: Text
+    elif jd_method == "Text":
+        jd_text = st.sidebar.text_area("Enter JD text")
+        jd_title = "n/a"
+        if jd_text:
+            st.write(f'Job Description: Text input')
+        return jd_title, jd_text
+
+
 
 
 def submit_button(resume_text, jd_title, jd_text, language):
+    if jd_text and resume_text:
+        st.write("Match!をクリックしてください。")
     # Submit button
     if st.sidebar.button("Match!"):
         output = compare_resume(resume_text, jd_title, jd_text, language)
