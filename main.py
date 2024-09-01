9b965a5d4ffcc9509f788f9a8a1f19a67a71f45a
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-09-01 11:09:00 +0900

    cleared unused imports

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import pdfplumber
-from src.resumematch_functions import compare_resume
-from src.JP_st_functions import read_resume, JP_UI, JP_resume_input, JP_jd_input, JP_submit_button
+from src.JP_st_functions import JP_UI
 from src.st_functions import UI
 
 def main():
