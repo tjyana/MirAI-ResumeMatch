0201a4f9db8cf7aae4f37da03a02953d968ae94e
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-09-18 00:30:32 +0900

    indent problem should be fixed

--- src/st_functions.py
+++ src/st_functions.py
@@ -54,7 +54,6 @@ def jd_input():
                 return jd_title, jd_text
             except Exception as e:
                 st.error("""Could not retrieve job information from the URL.
-
                          Please make sure the URL is for one JD from the Money Forward careers page: \n
                         Japanese: https://recruit.moneyforward.com/#job \n
     English: https://recruit.moneyforward.com/en/ \n
